dd9d92f5a69e9aac8ec8ae47b629840f26de990d
Rahul Singh <singh.rahul26@hotmail.com>
2020-04-09 10:15:11 +0530

    added functionality isChainValid

--- blockchain.js
+++ blockchain.js
@@ -3,27 +3,63 @@ const Block = require('./block');
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
   }
-
+  
+  // create genesis block
   createGenesisBlock () {
-    return new Block(Date.now(), "Genesis block", "0");
+    return new Block(0, Date.now(), "Genesis block", "0");
   }
-
+  
+  // return latest block of chain.
   getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
-
+  
+  // add a new block to chain
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
+  }
+  
+  // verify if chain is valid
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+      
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
-
 // module.exports = Blockchain
-let coin = new Blockchain();
-coin.addBlock(new Block(Date.now(), { amount: 4 }));
-coin.addBlock(new Block(Date.now(), { amount: 5 }));
-console.log(JSON.stringify(coin, null, 4));
-console.log('getLastBlock: ', coin.getLatestBlock());
+
+let sampleCoin = new Blockchain();
+console.log('Mining block 1...');
+sampleCoin.addBlock(new Block(1, Date.now(), { amount: 4 }));
+
+console.log('Mining block 2...');
+sampleCoin.addBlock(new Block(2, Date.now(), { amount: 6 }));
+console.log('Blockchain valid? ' + sampleCoin.isChainValid());
+
+// Trying to manipulate a block.
+console.log('Try changing a block...');
+sampleCoin.chain[1].data = { amount: 100 };
+
+// Verify if blockchain is valid or not.
+sampleCoin.chain[1].hash = sampleCoin.chain[1].calculateHash();
+console.log("Blockchain valid? " + sampleCoin.isChainValid());
+
+// Print blockchain data.
+console.log(JSON.stringify(sampleCoin, null, 4));
+// Print latest block.
+console.log('getLastBlock: ', sampleCoin.getLatestBlock());
