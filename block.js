98fab700f62bf763794d83601e0c7579ef6748a2
Rahul Singh <singh.rahul26@hotmail.com>
2020-04-09 09:57:44 +0530

    added mine block functionality.

--- block.js
+++ block.js
@@ -1,17 +1,26 @@
 const SHA256 = require("crypto-js/sha256");
 
 class Block {
-  constructor (timestamp, data, previousHash = '') {
-    this.previousHash = previousHash;
+  constructor (index, timestamp, data, previousHash = '') {
+    this.index = index;
     this.timestamp = timestamp;
     this.data = data;
-        
+    this.previousHash = previousHash;
     // When creating a new Block, automatically calculate its hash.
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
-    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+  
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("BLOCK MINED: " + this.hash);
   }
 }
 
